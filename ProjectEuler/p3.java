ba8c80933ee8ace3002d2edab2fc22dfc6478b47
Man Yiu Lam <manyiulam97@gmail.com>
2017-10-16 16:39:10 +0100

    Project Euler p3

---
+++ ProjectEuler/p3.java
@@ -0,0 +1,16 @@
+public class p3 {
+public static void main(String[]args) {
+	long num = 600851475143l;
+	double rt = Math.sqrt((double)num);
+	long sum = 1l; // cant mutliply by 0
+
+		for(int i = 2; i <= rt; i++) {
+			if(num%i == 0) {
+				sum *= i;
+
+		if(sum == num) System.out.print(i);
+			}
+		}
+
+    }
+}
