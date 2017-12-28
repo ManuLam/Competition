a10b96dcf4d02acb446da4ef64ef8a4970596a45
Man Yiu Lam <manyiulam97@gmail.com>
2017-12-28 03:29:14 +0000

    Kattis

--- Kattis/veci.py
+++ Kattis/veci.py
@@ -1,16 +1,10 @@
-x = [x for x in input()]
-a,s = [], 0
-for i in range(len(x)-1, 0, -1):
-  a.append(x[i])
-  if(x[i-1] < x[i]):
-    for j in sorted(a):
-      if(x[i-1] < j):
-        a.append(x[i-1])
-        x[i-1] = j
-        a.remove(j)
-        a.sort()
-        s = (''.join(x[:i])+''.join(a))
-        break
+a,s = [x for x in input()], 0
+for i in range(len(a)-1, 0, -1):
+  if(a[i-1] < a[i]):
+    b = sorted(a[i:])
+    for j in range(len(b)):
+      if(b[j] > a[i-1]): a[i-1],b[j] = b[j],a[i-1]; break
+    s = (''.join(a[:i]) + ''.join(b))
     break
-
+    
 print(s)
