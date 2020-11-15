5564ac362c18f663b6dca4a000615504fc5d4026
Man Yiu Lam <manyiulam97@gmail.com>
2020-11-15 15:11:26 +0000

    Kattis

---
+++ Kattis/honourThyApaxianParent.py
@@ -0,0 +1,13 @@
+Y,P = input().split()
+
+if Y[-1] == 'e':
+  print(Y + 'x' + P)
+
+elif Y[-1] in ['a', 'i', 'o', 'u']:
+  print(Y[:-1] + 'ex' + P)
+
+elif Y[-2:] == 'ex':
+  print(Y + P)
+
+else: 
+  print(Y + 'ex' + P)
