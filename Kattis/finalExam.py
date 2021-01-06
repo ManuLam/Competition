f81a5f34c4d2abbb60b25ae55f49e68718e9e397
Man Yiu Lam <manyiulam97@gmail.com>
2021-01-06 16:50:43 +0000

    Kattis

--- Kattis/finalExam.py
+++ Kattis/finalExam.py
@@ -1,19 +1,19 @@
 n = int(input())
-ans_array = [input() for _ in range(n)]
+ar = [input() for _ in range(n)]
 
 
-curr_letter = ans_array[0]
-points = 0
-ans_points = 0
+cur = ar[0]
+same = 0
+ans = 0
 
 for i in range(1, n):
-    if curr_letter == ans_array[i]:
-        points += 1
+    if cur == ar[i]:
+        same += 1
     
     else:
-        ans += max(0, points)
-        curr_letter = ans_array[i]
-        points = 0
+        ans += max(0, same)
+        cur = ar[i]
+        same = 0
     
-ans_points += max(0, points)    
-print(ans_points)
+ans += max(0, same)    
+print(ans)
